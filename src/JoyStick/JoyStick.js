d34fa33463534d6e77ebeda8775c5044d49e2e21
DANIEL CRITCHFIELD <danielcritchfield@DANIELs-MBP.hsd1.pa.comcast.net>
2020-01-08 23:12:48 -0500

    added draw base circle for joystick and log mouse movement

--- src/JoyStick/JoyStick.js
+++ src/JoyStick/JoyStick.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
 
+import './JoyStick.css';
+
 class JoyStick extends Component {
-    render() {
+
+    drawBase() {
+        const x = this.props.centerX;
+        const y = this.props.centerY;
+        
+        let style = {};
+        if (x !== 0 && y !== 0) {
+            style = {
+                position: 'absolute',
+                width: '100px',
+                height: '100px',
+                left: x-50,
+                top: y-50,
+                borderRadius: '50px',
+                backgroundColor: 'yellow'
+            }
+        }
         return (
-            <div className="JoyStick"></div>
+            <div style={style}></div>
         );
+
+    }
+
+    drawMovement() {
+        // TODO
+    }
+
+    render() {
+        return this.drawBase();
     }
 }
 
--- src/JoyStickPanel/JoyStickPanel.js
+++ src/JoyStickPanel/JoyStickPanel.js
@@ -11,24 +11,46 @@ class JoyStickPanel extends Component {
 
         this.state = {
             centerX: 0,
-            centerY: 0
+            centerY: 0,
+            mouseDown: false
         };
     }
 
     press = (event) => {
         this.setState( {
             centerX: event.center.x,
-            centerY: event.center.y
+            centerY: event.center.y,
+            mouseDown: true
         } );
-        console.log(this.state);
+    }
+
+    pressUp = () => {
+        this.setState( {
+            centerX: 0,
+            centerY: 0,
+            mouseDown: false
+        } );
+    }
+
+    mouseMove = (event) => {
+        if (this.state.mouseDown) {
+            console.log("X: " + event.clientX);
+            console.log("Y: " + event.clientY);
+
+            // TODO
+        }
     }
 
     render() {
         return (
             <div className="JoyStickPanel">
-                <Hammer onTap={this.press}>
-                    <div className="test">
-                        <div></div>
+                <Hammer onPress={this.press} onPressUp={this.pressUp} onMouseMove={this.mouseMove}>
+                    <div className="container">
+                        <JoyStick 
+                            centerX={this.state.centerX}
+                            centerY={this.state.centerY}
+
+                        />
                     </div>
                 </Hammer>
             </div>
