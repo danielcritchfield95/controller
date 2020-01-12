e39a1ca10d553df2877ea48ec6f7df38f206127c
DANIEL CRITCHFIELD <danielcritchfield@DANIELs-MBP.hsd1.pa.comcast.net>
2020-01-12 14:07:01 -0500

    used hamerjs library instead of react component for gesture panel to enable all swipe directions

--- src/GesturePanel/GesturePanel.js
+++ src/GesturePanel/GesturePanel.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Hammer from 'react-hammerjs';
+import Hammer from 'hammerjs';
 
 import './GesturePanel.css';
 
@@ -7,16 +7,16 @@ class GesturePanel extends Component {
 
     handleTap = (event) => {
         console.log("Tapped");
-        document.body.style.backgroundColor='aqua';
+        document.body.style.backgroundColor = 'aqua';
     }
 
     handleDoubleTap = (event) => {
         console.log("Double Tapped");
-        document.body.style.backgroundColor='orange';
+        document.body.style.backgroundColor = 'orange';
     }
 
     handlePinchIn = (event) => {
-        document.body.style.backgroundColor='yellow';
+        document.body.style.backgroundColor = 'yellow';
     }
 
     handleSwipeLeft = (event) => {
@@ -40,26 +40,51 @@ class GesturePanel extends Component {
 
         panel.style.backgroundColor = this.props.color;
         panel.style.width = this.props.width;
+
+        let hammer = new Hammer(panel);
+
+        // enable all directions
+        hammer.get('swipe').set({
+            direction: Hammer.DIRECTION_ALL,
+        });
+        
+        // enable pinch
+        hammer.get('pinch').set({
+            enable: true
+        });
+
+        hammer.on("tap", event => {
+            this.handleTap(event);
+        });
+
+        hammer.on("doubletap", event => {
+            this.handleDoubleTap(event);
+        });
+
+        hammer.on("swipeleft", event => {
+            this.handleSwipeLeft(event);
+        });
+
+        hammer.on("swiperight", event => {
+            this.handleSwipeRight(event);
+        });
+
+        hammer.on("swipeup", event => {
+            this.handleSwipeUp(event);
+        });
+
+        hammer.on("swipedown", event => {
+            this.handleSwipeDown(event);
+        });
+
+        hammer.on("pinchstart", event => {
+            this.handlePinchIn(event);
+        });
     }
 
     render() {
         return (
-            <Hammer
-                onTap={this.handleTap}
-                onDoubleTap={this.handleDoubleTap}
-                onPinchStart={this.handlePinchIn}
-                onSwipeRight={this.handleSwipeRight}
-                onSwipeLeft={this.handleSwipeLeft} 
-                onSwipeUp={this.handleSwipeUp}
-                onSwipeDown={this.handleSwipeDown}
-                options={{
-                    recognizers: {
-                       pinch: { enable: true }
-                    }
-                 }}
-                 >
-                <canvas className="GesturePanel"></canvas>
-            </Hammer>
+            <canvas className="GesturePanel"></canvas>
         );
     }
 }
