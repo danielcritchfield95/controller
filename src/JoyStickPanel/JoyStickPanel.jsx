14ee21a52dc366966f375ce09f5cf6db5c3b169b
tbobik <tbobik@robomatter.com>
2020-01-24 14:52:34 -0500

    using props in the joystick panel

--- src/JoyStickPanel/JoyStickPanel.jsx
+++ src/JoyStickPanel/JoyStickPanel.jsx
@@ -19,74 +19,71 @@ class JoyStickPanel extends Component {
     }
 
     handleMouseDown = (event) => {
-        this.setState( {
+        this.setState({
             centerX: event.clientX,
             centerY: event.clientY,
             active: true
-        } );
+        });
     }
 
     handleMouseUp = (event) => {
-        this.setState( {
+        this.setState({
             centerX: 0,
             centerY: 0,
             active: false,
             mouseX: 0,
             mouseY: 0
-        } );
+        });
     }
 
     handleMouseMove = (event) => {
         if (this.state.active) {
-            this.setState( {
+            this.setState({
                 mouseX: event.clientX,
                 mouseY: event.clientY
-            } );
+            });
         }
     }
 
     handleTouchStart = (event) => {
         const touch = event.targetTouches[0];
-        this.setState( {
+        this.setState({
             centerX: touch.clientX,
             centerY: touch.clientY,
             active: true
-        } );
+        });
     }
 
     handleTouchEnd = () => {
-        this.setState( {
+        this.setState({
             centerX: 0,
             centerY: 0,
             active: false,
             mouseX: 0,
             mouseY: 0
-        } );
+        });
     }
 
     handleTouchMove = (event) => {
         const touch = event.targetTouches[0];
         if (this.state.active) {
-            this.setState( {
+            this.setState({
                 mouseX: touch.clientX,
                 mouseY: touch.clientY
-            } );
+            });
         }
     }
 
-    componentDidMount() {
-        const panel = document.querySelector('.JoyStickPanel');
-
-        panel.style.backgroundColor = this.props.color;
-        panel.style.width = this.props.width;
-    }
-
     render() {
+        const style = {
+            backgroundColor: this.props.color,
+            width: this.props.width
+        }
         return (
-            <div className="JoyStickPanel">
+            <div className="JoyStickPanel" style={style}>
                 <Hammer onMouseDown={this.handleMouseDown} onMouseUp={this.handleMouseUp} onMouseMove={this.handleMouseMove} onTouchStart={this.handleTouchStart} onTouchEnd={this.handleTouchEnd} onTouchMove={this.handleTouchMove}>
                     <div className="container">
-                        <JoyStick 
+                        <JoyStick
                             centerX={this.state.centerX}
                             centerY={this.state.centerY}
                             mouseX={this.state.mouseX}
@@ -94,6 +91,7 @@ class JoyStickPanel extends Component {
                             baseColor={this.props.joystickBaseColor}
                             movementColor={this.props.joystickMovementColor}
                             handleJoyStickData={this.props.handleJoyStickData} />
+
                     </div>
                 </Hammer>
             </div>
